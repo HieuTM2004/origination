15037aeef99e5d8e3c36cfbb550eb9a5af68c4dc
hieuTran <tranminhhieu291104@gmail.com>
2025-07-29 09:43:53 +0700

    add all project files

---
+++ src/main/java/com/kane/origination/OriginationApplication.java
@@ -0,0 +1,13 @@
+package com.kane.origination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kane/origination/OriginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kane.origination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
